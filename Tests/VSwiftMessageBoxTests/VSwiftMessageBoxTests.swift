6970a2d69d2f0d4ff731f4e45d70ec418c6ba093
vong-e <sinkim123321@gmail.com>
2022-02-19 22:48:00 +0900

    support spm

---
+++ Tests/VSwiftMessageBoxTests/VSwiftMessageBoxTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VSwiftMessageBox
+
+final class VSwiftMessageBoxTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VSwiftMessageBox().text, "Hello, World!")
+    }
+}
